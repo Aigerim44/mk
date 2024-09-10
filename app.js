9a4b0ff319dc89a15e449c2dbada1d6ad16d8b01
azatmak00 <azatmaketaev01@gmail.com>
2024-09-11 03:54:45 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+let input = document.querySelector("input")
+let button=document.querySelector("button")
+
+button.onclick=()=>{
+    document.body.style.background=input.value
+    input.value
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
